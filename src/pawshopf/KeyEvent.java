2ef77894b06b94cbe9a8ae4c2e6bef9a8ff179f1
Lasantha Wijewardhana <lasanthakumara738@gmail.com>
2025-05-06 19:00:33 +0530

    Initial commit

---
+++ src/pawshopf/KeyEvent.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pawshopf;
+
+/**
+ *
+ * @author lasan
+ */
+class KeyEvent {
+
+    static int VK_Enter;
+    static int VK_ENTER;
+    
+}
